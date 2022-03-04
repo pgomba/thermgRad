4a0557d7693dd338ff9f2c11c8b6ac9e41b7c35e
pgomba <95383762+pgomba@users.noreply.github.com>
2022-03-04 13:28:20 +0000

    functional package

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(thermgRad)
+
+test_check("thermgRad")
---
+++ tests/testthat/test-coolbear.R
@@ -0,0 +1,5 @@
+test_that("T50 ", {
+  expect_equal(coolbear(seq(1,10,1),c(0,0,0,0,0,6,10,15,18,20),20),7)
+  expect_equal(round(coolbear(seq(1,10,1),c(0,0,0,0,0,6,12,15,18,20),20),3),6.667)
+  expect_equal(coolbear(seq(1,10,1),c(0,0,0,0,0,6,6,6,6,9),20),"NA")
+})
