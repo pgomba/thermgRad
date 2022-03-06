2c1f3c49c708eeedbc7240f1bb319f8ccf5e4c67
pgomba <95383762+pgomba@users.noreply.github.com>
2022-03-06 16:27:46 +0000

    added temperature fluctuation output

--- R/tempgrad.R
+++ R/tempgrad.R
@@ -1,6 +1,6 @@
 #' Generate temperature gradient grid
 #'
-#' Taking into account thermal gradient plate corner temperatures this function return a data frame with the temperatures of each petri dish. Using the "precise" method returns a more accurate estimate of temperatures across the thermal plate, while with the adjust parameter is possible to estimate the temperature at the center of each Petri dish
+#' Taking into account thermal gradient plate corner temperatures this function return a data frame with day and night temperatures for each petri dish, their average temperature and the temperature fluctuation. Using the "precise" method returns a more accurate estimate of temperatures across the thermal plate, while with the adjust parameter is possible to estimate the temperature at the center of each Petri dish
 #'
 #' @param dayBL Average diurnal temperature at the bottom left side of the thermal gradient plate
 #' @param dayBR Average diurnal temperature at the bottom left side of the thermal gradient plate
@@ -14,7 +14,7 @@
 #' @param method Leave blank to use average corner temperature values or use "precise" to create a temperature gradient based in individual corner temperatures
 #' @param adjust tbd, but will be used to center temperature into Petri dish
 #'
-#' @return A dataframe with day and night temperatures for each Petri dish
+#' @return A data frame with day and night temperatures, average temperature and temperature fluctuation for each Petri dish
 #' @export
 #'
 #' @examples
@@ -47,11 +47,11 @@ tempgrad<-function(dayBL="Diurnal bottom left temperature",
       v_day <- c(v_day, day_temp)
       v_night<-c(v_night,night_temp)
     }
-    data.frame(PD_ID=grid,day_temp=v_day,night_temp=v_night)
+    data.frame(PD_ID=grid,day_temp=v_day,night_temp=v_night,average=(v_day+v_night)/2,fluctuation=round(abs(v_day-v_night),2))
   }
   else{
   day_temp<-rep(seq((dayTL+dayTR)/2,(dayBL+dayBR)/2,length.out=petri),time=petri)
   night_temp<-rep(seq((nightBL+nightTL)/2,(nightBR+nightTR)/2,length.out=petri),each=petri)
-  data.frame(PD_ID=grid,day_temp=day_temp,night_temp=night_temp)
+  data.frame(PD_ID=grid,day_temp=day_temp,night_temp=night_temp,average=(day_temp+night_temp)/2,fluctuation=round(abs(day_temp-night_temp),2))
   }
 }
