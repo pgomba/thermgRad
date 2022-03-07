b386d7ec078f0e29a75515902290348ed62ef8c4
pgomba <95383762+pgomba@users.noreply.github.com>
2022-03-07 22:52:47 +0000

    update to example function

--- R/tg_example.R
+++ R/tg_example.R
@@ -2,8 +2,6 @@
 #'
 #' Loads a dataframe which can be used as an example to try out the functions
 #'
-#' @param x a placeholder
-#'
 #' @return an example data frame
 #' @export
 #'
@@ -12,7 +10,7 @@
 #' @examples
 #' df<-tg_example()
 #'
-tg_example<-function(x="leave blank for default example"){
+tg_example<-function(){
   x<-"https://raw.githubusercontent.com/pgomba/thermgRad/main/data-raw/tg_example.csv?token=GHSAT0AAAAAABRE3LFF7X74F7VPT2P2SNAGYRGROSA"
   read_delim(x, delim = ";",escape_double = FALSE, trim_ws = TRUE)
 
