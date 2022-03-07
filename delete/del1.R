b28a6a9554f13bb633796ff1f43ddc1457141ef4
pgomba <95383762+pgomba@users.noreply.github.com>
2022-03-07 22:19:53 +0000

    update

--- delete/del1.R
+++ delete/del1.R
@@ -3,23 +3,19 @@ library(ggplot2)
 library(magrittr)
 library(dplyr)
 
-chopchop(temp2)
+build()
 
 document()
-?plot_tempgrad
+
 check()
+?devtools::check
+
 install()
+
 library(thermgRad)
-
-a<-tempgrad(dayBL=0,dayBR=0,dayTL=40,dayTR=40,nightBL=0,nightBR=40,nightTL=0,nightTR=40,petri=13)
-library(thermgRad)
-
-tempgrad(dayBL=5.5,dayBR=6.1,dayTL=40,dayTR=39,nightBL=6,nightBR=39,nightTL=6.2,nightTR=40,petri=13,method = "precise")%>%
-  plot_tempgrad(toplot = "average")
-
-rep(seq((dayTL+dayTR)/2,(dayBL+dayBR)/2,length.out=petri),time=petri)
-
-plot_tempgrad(a,toplot = "average")
 
 use_pipe()
 use_github_actions()
+
+usethis::use_data_raw(name = 'tg_example')
+build_manual(path = getwd())
