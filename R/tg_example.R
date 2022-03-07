b4bfc7349aa4d71b99c519dbcea919c9489172b0
pgomba <95383762+pgomba@users.noreply.github.com>
2022-03-07 22:52:20 +0000

    function to upload example

---
+++ R/tg_example.R
@@ -0,0 +1,20 @@
+#' Load example
+#'
+#' Loads a dataframe which can be used as an example to try out the functions
+#'
+#' @param x a placeholder
+#'
+#' @return an example data frame
+#' @export
+#'
+#' @importFrom readr read_delim
+#'
+#' @examples
+#' df<-tg_example()
+#'
+tg_example<-function(x="leave blank for default example"){
+  x<-"https://raw.githubusercontent.com/pgomba/thermgRad/main/data-raw/tg_example.csv?token=GHSAT0AAAAAABRE3LFF7X74F7VPT2P2SNAGYRGROSA"
+  read_delim(x, delim = ";",escape_double = FALSE, trim_ws = TRUE)
+
+}
+
