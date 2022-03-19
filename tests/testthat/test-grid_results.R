53793dbf26805d5594b0b0c47e3aa79802413f29
pgomba <95383762+pgomba@users.noreply.github.com>
2022-03-19 22:02:53 +0000

    grid_result test improvements

--- tests/testthat/test-grid_results.R
+++ tests/testthat/test-grid_results.R
@@ -1,27 +1,12 @@
 #input
-
 data<-thermgRad::tg_example
-petri<-13
-dayBL<-5
-dayBR<-7
-dayTL<-40
-dayTR<-39
-nightBL<-5
-nightBR<-39
-nightTL<-7
-nightTR<-39
-
-#functions
-let<-rep(LETTERS[seq(from=1,to=petri)],each=petri)
-grid<-paste0(let,1:petri)
-n_horiz_down<-((abs(nightBL-nightBR)/petri)*-sign(nightBL-nightBR))/2
-n_verti_right<-((abs(nightBR-nightTR)/petri)*-sign(nightBR-nightTR))/2
-d_verti_left<-((abs(dayBL-dayTL)/petri)*-sign(dayBL-dayTL))/2
-nBR<-nightBR-n_horiz_down+n_verti_right
 
 test_that("grid_results works", {
-  expect_equal(let[8], "A")
-  expect_equal(grid[21],"B8")
-  expect_equal(d_verti_left,1.346154, tolerance=0.000001)
-  expect_equal(nBR,37.69231, tolerance=0.00001)
-    })
+  expect_equal(grid_results(data, 0,3,40,38,0,38,2,39, petri=13)[5,5], 25.5)
+  expect_equal(grid_results(data, 0,3,40,38,0,38,2,39, petri=13)[10,4], 5.9375)
+  expect_equal(grid_results(data, 0,3,40,38,0,38,2,39, petri=13,method="precise")[10,4], 5.25)
+  expect_equal(grid_results(data, 0,3,40,38,0,38,2,39,method="precise",adjust=TRUE, petri=13)[43,3], 11.49519,tolerance = 0.00001)
+
+
+  })
+5.9375
