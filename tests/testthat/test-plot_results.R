b8e88ff9364c0d5f48dbc45122f66488bdb3ebff
pgomba <95383762+pgomba@users.noreply.github.com>
2022-03-19 22:36:11 +0000

    plot_result test improvements

--- tests/testthat/test-plot_results.R
+++ tests/testthat/test-plot_results.R
@@ -1,5 +1,16 @@
 data<-thermgRad::tg_example
-germin<-((data[ncol(data)-1])/(data[ncol(data)]))*100
-test_that("plot results is doing fine", {
-  expect_equal(nrow(germin), 169)
+
+test_that("plot_results works", {
+  plot<-plot_results(data,3,6,40,38,4,39,7,40,petri=13,method = "precise",toplot="average")
+  expect_equal(plot$data$fluc[5], 21.67)
+  expect_equal(plot$labels$title, "Average temperature")
+
+  plot2<-plot_results(data,3,6,40,38,4,39,7,40,petri=13,method = "precise",toplot="fluctuation")
+  expect_equal(plot2$data$germ[20], 89.47368,tolerance = 0.00001)
+  expect_equal(plot2$labels$title, "Temperature fluctuation")
+
+  plot3<-plot_results(data,3,6,40,38,4,39,7,40,petri=13,method = "precise",adjust=TRUE,toplot="germina")
+  expect_equal(plot3$data$PD_ID[28], "C2")
+  expect_equal(plot3$labels$title, "Germination %")
+
 })
