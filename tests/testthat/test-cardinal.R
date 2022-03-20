6bbdd9883639260b80e9cb00d79c7cc7b4c60a06
pgomba <95383762+pgomba@users.noreply.github.com>
2022-03-20 13:11:49 +0000

    test update

--- tests/testthat/test-cardinal.R
+++ tests/testthat/test-cardinal.R
@@ -1,4 +1,8 @@
+library(testthat)
+
 data<-thermgRad::tg_example
+
+selectGR<-7
 
 test_that("cardinal works", {
   expect_equal(thermgRad::cardinal(data,0,0,40,40,0,40,0,40,petri = 13,fs=0,fe=3), 'To = 20.12')
